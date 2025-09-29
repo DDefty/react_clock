9f8adfac137a43770ecde74295b657b8e49322e4
DDefty <dawid.kogut99@gmail.com>
2025-09-29 14:08:53 +0200

    Add Clock component and integrate with App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.scss';
+import { Clock } from './Clock';
 
 function getRandomName(): string {
   const value = Date.now().toString().slice(-4);
@@ -7,31 +8,59 @@ function getRandomName(): string {
   return `Clock-${value}`;
 }
 
-export const App: React.FC = () => {
-  const today = new Date();
-  let clockName = 'Clock-0';
-
-  // This code starts a timer
-  const timerId = window.setInterval(() => {
-    clockName = getRandomName();
-  }, 3300);
-
-  // this code stops the timer
-  window.clearInterval(timerId);
-
-  return (
-    <div className="App">
-      <h1>React clock</h1>
-
-      <div className="Clock">
-        <strong className="Clock__name">{clockName}</strong>
-
-        {' time is '}
-
-        <span className="Clock__time">
-          {today.toUTCString().slice(-12, -4)}
-        </span>
-      </div>
-    </div>
-  );
+type AppState = {
+  clockName: string;
+  hasClock: boolean;
 };
+
+export class App extends React.Component<{}, AppState> {
+  nameTimerId?: number;
+
+  contextHandler = (event: MouseEvent) => {
+    event.preventDefault(); // not to show the context menu
+
+    this.setState({ hasClock: false });
+  };
+
+  clickHandler = () => {
+    this.setState({ hasClock: true });
+  };
+
+  state = {
+    clockName: 'Clock-0',
+    hasClock: true,
+  };
+
+  componentDidMount() {
+    // start name updater
+    this.nameTimerId = window.setInterval(() => {
+      this.setState({ clockName: getRandomName() });
+    }, 3300);
+
+    document.addEventListener('contextmenu', this.contextHandler);
+    document.addEventListener('click', this.clickHandler);
+  }
+
+  componentWillUnmount() {
+    if (this.nameTimerId) {
+      clearInterval(this.nameTimerId);
+    }
+
+    document.removeEventListener('contextmenu', this.contextHandler);
+    document.removeEventListener('click', this.clickHandler);
+  }
+
+  componentDidUpdate(): void {
+    // rename logging is handled by the Clock component's componentDidUpdate
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>React clock</h1>
+
+        {this.state.hasClock ? <Clock name={this.state.clockName} /> : null}
+      </div>
+    );
+  }
+}
